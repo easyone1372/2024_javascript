8c2d2eddfd0cee8d8de021ee37b2be61d1399a2a
easyone1372 <74178762+easyone1372@users.noreply.github.com>
2024-05-21 18:21:23 +0900

    2024.05.21 update

--- day06/quiz/index.js
+++ day06/quiz/index.js
@@ -3,15 +3,45 @@
 //테두리 사이즈 3개
 
 const box = document.querySelector(".box");
-const colorBtn = document.querySelector(".colorBtn");
-const sizeBtn = document.querySelector(".sizeBtn");
-const radiusBtn = document.querySelector(".radiusBtn");
+const colorBtnBox = document.querySelector(".colorBtn_box");
+const sizeBtnBox = document.querySelector(".sizeBtn_box");
+const radiusBtnBox = document.querySelector(".radiusBtn_box");
 
-// const colorArr = ["red", "yellow", "green", "blue", "purple"];
-// const sizeArr = ["XS", "S", "M", "L", "XL"];
-// const radiusArr = ["0", "10", "50"];
-// const colorArr = ["빨간색", "노랑색", "초록색", "파랑색", "보라색"];
+const colorArr = ["red", "yellow", "green", "blue", "purple"];
+const sizeHng = ["xsmall", "small", "medium", "large", "xlarge"];
+const radiusArr = ["radiusZero", "radiusSmooth", "radiusRound"];
 
-colorBtn.addEventListener("click", () => {
-  const a = colorBtn.innerText;
+// const colorHng = ["빨강색", "노랑색", "초록색", "파랑색", "보라색"];
+
+colorArr.forEach((v) => {
+  const colorBtn = document.createElement("button");
+  colorBtn.innerText = v;
+  colorBtn.addEventListener("click", () => {
+    const colorText = colorBtn.innerText;
+    box.classList.remove("red", "yellow", "green", "blue", "purple");
+    box.classList.add(`${colorText}`);
+  });
+  colorBtnBox.appendChild(colorBtn);
+});
+
+sizeHng.forEach((v) => {
+  const sizeBtn = document.createElement("button");
+  sizeBtn.innerText = v;
+  sizeBtn.addEventListener("click", () => {
+    const sizeText = sizeBtn.innerText;
+    box.classList.remove("xsmall", "small", "medium", "large", "xlarge");
+    box.classList.add(`${sizeText}`);
+  });
+  sizeBtnBox.appendChild(sizeBtn);
+});
+
+radiusArr.forEach((v) => {
+  const radiusBtn = document.createElement("button");
+  radiusBtn.innerText = v;
+  radiusBtn.addEventListener("click", (v) => {
+    const radiusText = radiusBtn.innerText;
+    box.classList.remove("radiusZero", "radiusSmooth", "radiusRound");
+    box.classList.add(`${radiusText}`);
+  });
+  radiusBtnBox.appendChild(radiusBtn);
 });
