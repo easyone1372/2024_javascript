fb197bc26af0f98bf40e7d1a61879f9f8a82ed5a
easyone1372 <74178762+easyone1372@users.noreply.github.com>
2024-05-23 12:50:16 +0900

    2024.05.23

--- day08/signup/index.js
+++ day08/signup/index.js
@@ -1,15 +1,11 @@
 const email = document.querySelector(".email");
-const passWord = document.querySelector(".passWord");
-const passwordCheck = document.querySelector(".passWordCheck");
-const pwBox = document.querySelector("passWordbox");
-const pwchBox = document.querySelector("passWordCheckbox");
+const passWord = document.querySelector(".passwordInputBox");
+const passwordCheck = document.querySelector(".passwordInputCheckBox");
 const showText = document.querySelector(".showText");
 const open = document.querySelector(".open");
 const close = document.querySelector(".close");
 
 const checkList = [email, passWord, passwordCheck];
-pwBox.close.style.display = "none";
-pwchBox.close.style.display = "none";
 
 email.addEventListener(
   ("input",
@@ -23,29 +19,3 @@ email.addEventListener(
       : email.classList.remove("checked");
   })
 );
-
-passWord.addEventListener("input", (e) => {
-  let a = e.target.value;
-  a.length >= 8
-    ? passWord.classList.add("checked")
-    : passWord.classList.remove("checked");
-});
-
-passwordCheck.addEventListener("input", (e) => {
-  let a = e.target.value;
-  a == passWord.target.value
-    ? passwordCheck.classList.add("checked")
-    : passwordCheck.classList.remove("checked");
-});
-
-open.addEventListener("click", () => {
-  showText.type = "text";
-  open.style.display = "none";
-  close.style.display = "block";
-});
-
-close.addEventListener("click", () => {
-  showText.type = "password";
-  open.style.display = "block";
-  close.style.display = "none";
-});
